6b10eafdb630a501236c2a78e4ec76dce896282b
Ale <98957312+vanuzita@users.noreply.github.com>
2022-05-16 12:46:34 -0300

    Add files via upload

---
+++ nuevocodigo.js
@@ -0,0 +1,6 @@
+console.log("¡Hola Mundo! Este es mi primer Script");
+document.write("Ejemplos de cómo incluir JavaScript en un documento HTML")
+
+
+alert("Ver lo simple que es agregar \"comillas dobles\" +   y \‘comillas simples’");
+
